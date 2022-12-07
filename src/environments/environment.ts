3fa71f36ec54dd533c3e5b3f740d20edb303443f
ffjjgih <lamcaobui26@gmail.com>
2022-12-07 10:56:55 +0700

    transaction bill offline

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,7 @@
 export const environment = {
   production: false,
   baseApi: 'http://localhost:1507/vibee/api/v1/',
+  // baseApi: 'https://6ac20b190b08d1.lhr.life:80/vibee/api/v1/',
   // baseApi: 'http://localhost:1507/',
   baseApiAdmin: 'http://localhost:1507/vibee/api/v1/admins/',
   base: 'http://localhost:1507/'
