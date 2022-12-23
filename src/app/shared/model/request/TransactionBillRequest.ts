aa27798f203188f2b440084b69d7e514465d384a
ffjjgih <lamcaobui26@gmail.com>
2022-12-24 02:04:42 +0700

    transaction bill offline

--- src/app/shared/model/request/TransactionBillRequest.ts
+++ src/app/shared/model/request/TransactionBillRequest.ts
@@ -1,9 +1,10 @@
 import {BaseRequest} from "./BaseRequest";
+import {ViewStallResult} from "../response/ViewStallResult";
 
 export class TransactionBillRequest extends BaseRequest{
   inPrice!: number;
   paymentMethod!: string;
   transactionType!: string;
   cartCode!: string;
-
+  viewStallResults: ViewStallResult[]=[];
 }
